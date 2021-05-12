fe9e0ff1203c0bc489951dfe157f376630082a2c
NitaRay <superlightwing@gmail.com>
2021-05-12 19:09:10 +0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require('apollo-server');
+
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is information`
+  }
+}
+
+// 3
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
